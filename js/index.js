72457cc78d20e7eb0d2a06dc68e1decc16766e87
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-01 14:36:28 -0700

    onpointerdown added as 7th, drag and drop was too complicated for today

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@
 const mainImg = document.querySelector('.intro img');
 console.log(mainImg);
 
-mainImg.addEventListener("dblclick", () => {
-    mainImg.style.display = "none";
+mainImg.addEventListener("dblclick", (e) => {
+    e.target.style.display = "none";
 });
 
 // document.querySelector('.intro img').addEventListener('dblclick', (e) => {
@@ -20,10 +20,12 @@ console.log(navie);
 navie.forEach(element => {
     element.addEventListener('mouseover', (event) => {
         event.target.style.fontSize = "4rem";
+        event.preventDefault();
     }) 
     element.addEventListener('mouseleave', (event) => {
         event.target.style.fontSize = "2rem";
     }) 
+    
 })
 
 
@@ -62,4 +64,55 @@ window.addEventListener("scroll", () =>{
     secondImg.style.transform = "translateY(-500px)";
 })
 
+////////////////BODY ART: ONPOINTERDOWN///////////
+document.querySelector('body').onpointerdown = (e) => {
+        e.target.style.backgroundColor = "red";
+}
 
+
+
+
+
+
+////////////////ISLAND GETS AWAY//////////
+
+// var dragged 
+
+// let draggable = document.querySelector('.draggable');
+// console.log(draggable)
+
+// document.addEventListener('drag', () => {
+
+// }, false);
+
+// document.addEventListener('dragstart', (event) => {
+//     dragged = event.target;
+// }, false)
+
+// document.addEventListener("dragover", function(event) {
+//     // prevent default to allow drop
+//     event.preventDefault();
+//   }, false);
+
+//   document.addEventListener("dragenter", function(event) {
+//     // highlight potential drop target when the draggable element enters it
+//     if (event.target.className == "destination") {
+//       event.target.style.background = "purple";
+//     }
+// }, false);
+
+//     document.addEventListener("dragleave", function(event) {
+//         // reset background of potential drop target when the draggable element leaves it
+//         if (event.target.className == "destination") {
+//           event.target.style.background = "";
+//         }
+      
+//       }, false);
+
+// document.addEventListener('drop', () => {
+//     if (event.target.className == "destination") {
+//         event.target.style.background = "";
+//         dragged.parentNode.removeChild( dragged );
+//         event.target.appendChild( dragged );
+//       }
+// }, false);
